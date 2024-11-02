23dc0a35e9b5e7c78b2b414cf6fa4cd7a5c63352
Isabel Katherine Morocho Marizaca <isabel.morocho@unl.edu.ec>
2024-11-02 13:41:52 -0500

    Agrego estructura base del proyecto

---
+++ src/main/java/com/sistemaFarmacia/rest/controller/dao/implement/InterfazDao.java
@@ -0,0 +1,10 @@
+package com.sistemaDeFarmacia.rest.controller.dao.implement;
+
+import com.sistemaDeFarmacia.rest.controller.tda.list.LinkedList;
+
+public interface InterfazDao <T> {
+    public void persist(T object) throws Exception;
+    public void merge(T object, Integer index) throws Exception;
+    public LinkedList listAll();
+    public T get(Integer id) throws Exception;    
+}
