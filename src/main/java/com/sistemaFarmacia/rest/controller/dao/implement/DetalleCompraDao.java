cc7f9e5dd7485c3b681c8af3a9eccc99934d1819
Cristopher David Chingo Rivera <166523123+DavidCh02@users.noreply.github.com>
2024-11-03 13:01:58 -0500

    update de codigo

--- src/main/java/com/sistemaFarmacia/rest/controller/dao/implement/DetalleCompraDao.java
+++ src/main/java/com/sistemaFarmacia/rest/controller/dao/implement/DetalleCompraDao.java
@@ -2,7 +2,7 @@ package com.sistemaFarmacia.rest.controller.dao.implement;
 
 import com.sistemaFarmacia.rest.models.DetalleCompra;
 
-public class DetalleCompraDao extends AdapterDao<DetalleCompra> {
+public class DetalleCompraDao extends com.sistemaDeFarmacia.rest.controller.dao.implement.AdapterDao<DetalleCompra> {
     private static DetalleCompraDao instance;
 
     public DetalleCompraDao() {
