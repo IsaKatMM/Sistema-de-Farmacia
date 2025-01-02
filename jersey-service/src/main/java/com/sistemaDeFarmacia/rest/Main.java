5652ca41a6e57eb23c0287b9af16afa019a06cf2
IsaKatMM <isabel.morocho@unl.edu.ec>
2025-01-02 12:53:00 -0500

    comit arreglo main/master

--- jersey-service/src/main/java/com/sistemaDeFarmacia/rest/Main.java
+++ jersey-service/src/main/java/com/sistemaDeFarmacia/rest/Main.java
@@ -22,7 +22,7 @@ public class Main {
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in com.sistemaFarmacia.rest package
-        final ResourceConfig rc = new ResourceConfig().packages("com.sistemaFarmacia.rest");
+        final ResourceConfig rc = new ResourceConfig().packages("com.sistemaDeFarmacia.rest");
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
