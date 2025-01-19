c29f0220cf0e15d9eb4a5c77ea295fae3594b28a
Santiago <santiago.d.jimenez@unl.edu.ec>
2025-01-19 13:06:31 -0500

    Correcion Frontend

--- FrontEnd/routes/route.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, abort, jsonify, request, render_template, redirect,url_for, flash
-import requests
-router = Blueprint('router', __name__)
-
-@router.route('/')
-def index():
-    return render_template('home.html')
-
